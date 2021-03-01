7ed97cf0be01f781fef22c2a1b70ecd21841be64
jglez <jglez.cs@gmail.com>
2021-03-01 13:07:59 -0600

    Added routing for components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import Dashboard from './components/Dashboard'
+import Signup from './components/Signup'
+import Login from './components/Login'
+import { Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+
+      <Route exact path='/'>
+        <Home />
+      </Route>
+
+      <Route exact path='/dash'>
+        <Dashboard />
+      </Route>
+
+      <Route exact path='/signup'>
+        <Signup />
+      </Route>
+
+      <Route exact path='/login'>
+        <Login />
+      </Route>
     </>
   );
 }
