1a8052d726030e630804777d23417e852b016d69
Nick Allen <nwestallen@gmail.com>
2021-03-02 17:16:55 -0600

    invite page rsvp & redirect

--- src/components/Invite.js
+++ src/components/Invite.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const initalData = {
@@ -13,6 +13,8 @@ const Invite = () => {
     const { potluckid } = useParams();
     const userID = localStorage.getItem('userID');
     const [potluckData, setPotluckData] = useState(initalData);
+    const history = useHistory();
+    const [success, setSuccess] = useState('');
 
     useEffect(() => {
         axiosWithAuth()
@@ -24,14 +26,40 @@ const Invite = () => {
             .catch(err => console.log(err));
     },[]);
 
+    const rsvp = e => {
+        e.preventDefault();
+        if (localStorage.getItem('token')) {
+            axiosWithAuth()
+                .post(`api/users/${userID}/potlucks/${potluckid}`)
+                .then(setSuccess("You have successfully RSVP'd"))
+                .catch(err => console.log(err));
+        } else {
+            history.push('/login');
+        }
+    };
+
+    const routeToDashboard = e => {
+        e.preventDefault();
+        history.push('/dash')
+    };
+
+    const routeToPotluck = e => {
+        e.preventDefault();
+        history.push(`/potluck/${potluckid}`)
+    };
+
     return (
         <div>
             <h1>Join my Potluck!</h1>
-            <p>What?: {potluckData.potluckname}</p>
-            <p>When?: {potluckData.date}, {potluckData.time}</p>
-            <p>Where?: {potluckData.location}</p>
-            <p>Hosted by {potluckData.organizer}</p>
-            <button>RSVP</button>
+            <div className='invitation'>
+                <p>What?: {potluckData.potluckname}</p>
+                <p>When?: {potluckData.date}, {potluckData.time}</p>
+                <p>Where?: {potluckData.location}</p>
+                <p>Hosted by {potluckData.organizer}</p>
+                {success || <button onClick={rsvp}>RSVP</button>}
+                {success && <button onClick={routeToDashboard}>Go To Dashboard</button>}
+                {success && <button onClick={routeToPotluck}>Go To Potluck</button>}
+            </div>
         </div>
     )
 }
